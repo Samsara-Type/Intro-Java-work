1ad27df03c10cf99a2b44c61882b0f41f00c06e2
Install Wizard <84146248+Samsara-Type@users.noreply.github.com>
2021-05-27 20:33:51 -0400

    Add files via upload

---
+++ Boringwindow.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Boringwindow extends JFrame
+{
+	public static void main( String[] args )
+	{
+		JFrame f = new Boringwindow();
+		f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		f.setSize(300, 200);
+		f.setVisible(true);
+		
+
+		
+	}
+} 
